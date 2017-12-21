31531d6fe2fb0977266d9731d5e8775d3d0b3064
tvoysosed <32384768+tvoysosed@users.noreply.github.com>
2017-12-22 02:53:46 +0300

    Update 7laba.cpp

--- 7laba.cpp
+++ 7laba.cpp
@@ -1,30 +1,36 @@
 #include <iostream> 
 #include <string> 
-
 using namespace std; 
- 
-struct Node { 
+
+struct Node
+{ 
 string str; 
 Node *next; 
-}; 
-int main() { 
+};
+ 
+int main() 
+{ 
 string str; 
 Node *first = new Node{str, nullptr}; 
 Node *last = nullptr; 
-while (true) { 
-cin >> str; 
-if (str == "end") break; 
-Node *curr = new Node{str, nullptr};
-if (first->next == nullptr) first->next = curr; 
-if (last != nullptr) { 
-last->next = curr; 
-} 
-last = curr; 
-} 
+while (true) 
+   { 
+   cin >> str; 
+   if (str == "end") break; 
+   Node *curr = new Node{str, nullptr}; 
+   if (first -> next == nullptr) first -> next = curr; 
+   if (last != nullptr)
+      { 
+      last->next = curr; 
+      }
+   last = curr; 
+   }
 Node *curr = nullptr; 
 curr = first; 
-do { 
-cout << curr->str << endl; 
-curr = curr->next;
-} while (curr != nullptr); 
+do
+   { 
+   cout << curr -> str << endl; 
+   curr = curr -> next;
+   }
+while (curr != nullptr); 
 }
